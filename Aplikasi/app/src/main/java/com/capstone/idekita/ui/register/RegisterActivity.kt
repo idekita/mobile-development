3eda308495c36dc994e39afaab13d398054bb239
WIN10 <reza_pb71@yahoo.com>
2023-05-25 00:08:14 +0700

    add register binding

--- Aplikasi/app/src/main/java/com/capstone/idekita/ui/register/RegisterActivity.kt
+++ Aplikasi/app/src/main/java/com/capstone/idekita/ui/register/RegisterActivity.kt
@@ -3,10 +3,14 @@ package com.capstone.idekita.ui.register
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.idekita.R
+import com.capstone.idekita.databinding.ActivityRegisterBinding
+import com.capstone.idekita.databinding.ActivitySplashScreenBinding
 
+private lateinit var binding: ActivityRegisterBinding
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
