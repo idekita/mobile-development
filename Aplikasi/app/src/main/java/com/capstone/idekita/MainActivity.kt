e23ca259fc3446780d62e3943674b7cd3cbf11e5
luzd <dzulfikri628@gmail.com>
2023-05-24 23:12:02 +0700

    namah dependencies ViewBinding

--- Aplikasi/app/src/main/java/com/capstone/idekita/MainActivity.kt
+++ Aplikasi/app/src/main/java/com/capstone/idekita/MainActivity.kt
@@ -2,8 +2,10 @@ package com.capstone.idekita
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.capstone.idekita.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+   // private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
