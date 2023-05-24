eda8b5717de738caa37f733ab958385a4259d054
luzd <dzulfikri628@gmail.com>
2023-05-25 00:01:54 +0700

    create register activity

---
+++ Aplikasi/app/src/main/java/com/capstone/idekita/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.idekita.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.idekita.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
