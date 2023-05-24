95e76949e1deed4b6aee89863e2a1a764bbd50c1
luzd <dzulfikri628@gmail.com>
2023-05-24 23:48:21 +0700

    create package ui

--- Aplikasi/app/src/main/java/com/capstone/idekita/ui/splashScreen/SplashScreenActivity.kt
+++ Aplikasi/app/src/main/java/com/capstone/idekita/ui/splashScreen/SplashScreenActivity.kt
@@ -1,14 +1,12 @@
-package com.capstone.idekita
+package com.capstone.idekita.ui.splashScreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.idekita.databinding.ActivityMainBinding
+import com.capstone.idekita.R
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_splash_screen)
     }
-
 }
