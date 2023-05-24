25e7ea00f5d0b2cbfc25be9ad8f764bef7fa8a0a
luzd <dzulfikri628@gmail.com>
2023-05-25 00:05:12 +0700

    add binding on splashscreen Activity

--- Aplikasi/app/src/main/java/com/capstone/idekita/ui/splashScreen/SplashScreenActivity.kt
+++ Aplikasi/app/src/main/java/com/capstone/idekita/ui/splashScreen/SplashScreenActivity.kt
@@ -3,10 +3,14 @@ package com.capstone.idekita.ui.splashScreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.idekita.R
+import com.capstone.idekita.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
