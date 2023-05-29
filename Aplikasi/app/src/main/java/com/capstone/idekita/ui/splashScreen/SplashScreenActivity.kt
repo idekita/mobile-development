d94549319fd4cedffc0dcca90a3dd292cfe43fea
WIN10 <reza_pb71@yahoo.com>
2023-05-29 19:30:48 +0700

    add splash screen

--- Aplikasi/app/src/main/java/com/capstone/idekita/ui/splashScreen/SplashScreenActivity.kt
+++ Aplikasi/app/src/main/java/com/capstone/idekita/ui/splashScreen/SplashScreenActivity.kt
@@ -1,16 +1,35 @@
 package com.capstone.idekita.ui.splashScreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.idekita.R
+import android.os.Handler
 import com.capstone.idekita.databinding.ActivitySplashScreenBinding
+import com.capstone.idekita.ui.login.LoginActivity
 
+
+@Suppress("DEPRECATION")
 class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            startActivity(
+                Intent(this@SplashScreenActivity, LoginActivity::class.java)
+            )
+            finish()
+        },DELAY)
+    }
+
+
+    companion object{
+        const val DELAY : Long = 2000
     }
 }
